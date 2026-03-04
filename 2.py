c807dfee935535f1c75c8b9e432f298e7da0293e
saisaharshnaidu29-cyber <saisaharshnaidu.29@gmail.com>
2026-03-04 16:50:53 +0400

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,19 @@
+name = "saharsh"
+age = 11
+is_student = True
+weight = 38.5
+print("name :", name)
+print("data type of name is :", type(name))
+print("Age :", age)
+print("data type of age is :", type(age))
+print("is_student :", is_student)
+print("data type of is_student is :", type(is_student))
+print("weight :", weight)
+print("data type of weight is :", type(weight))
+print("\n After type of casting....")
+age = age(str)
+print(age)
+print("the data type of age is :", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
