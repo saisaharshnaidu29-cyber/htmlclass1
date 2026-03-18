653ba6187f9370764fecef581ac706bd6304bef4
saisaharshnaidu29-cyber <saisaharshnaidu.29@gmail.com>
2026-03-18 16:53:36 +0400

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,19 +1,21 @@
-name = "saharsh"
-age = 11
-is_student = True
-weight = 38.5
-print("name :", name)
-print("data type of name is :", type(name))
-print("Age :", age)
-print("data type of age is :", type(age))
-print("is_student :", is_student)
-print("data type of is_student is :", type(is_student))
-print("weight :", weight)
-print("data type of weight is :", type(weight))
-print("\n After type of casting....")
-age = age(str)
-print(age)
-print("the data type of age is :", type(age))
-weight = int(weight)
-print(weight)
-print("Data type of weight is", type(weight))
+units = int(input("please enter the units you consumed:  "))
+if(units <= 50):
+    amount = units * 2.60
+    surcharge = 25
+
+
+elif(units <= 100):
+    amount = 130 + 162.50 + ((units - 100)* 5.26)
+    surcharge = 45
+
+
+elif(units <= 200):
+    amount = 130 + 162.50 + 526 + ((units - 200)* 8.45)
+    surcharge = 75
+
+else:
+    amount = 130 + 162.50 + 526 + ((units - 200) * 8.45)
+    surcharge = 75
+
+total = amount + surcharge
+print("\nElectricity Bill = %.2f"  %total)
